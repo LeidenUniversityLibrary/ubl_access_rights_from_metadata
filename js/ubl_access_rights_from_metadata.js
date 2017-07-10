7c9ab46184d961a8965b0364503fbac58f80cf77
lucasvanschaik <schaik@library.leidenuniv.nl>
2017-07-10 11:11:30 -0400

    Access restricted thumbnail from settings

--- js/ubl_access_rights_from_metadata.js
+++ js/ubl_access_rights_from_metadata.js
@@ -3,25 +3,34 @@
  * js/ubl_access_rights_from_metadata.js
  */
 
-jQuery(document).ready(function() {
-  var replaceInaccessibleThumbnail = function() {
-    var parts = this.src.split("?", 2);
-    if (parts[0].endsWith('datastream/TN/view')) {
-      var newsrc = '/sites/all/modules/ubl_access_rights_from_metadata/images/tn_restricted_access.gif';
-      if (parts.length == 2) {
-        newsrc += '?' + parts[1];
-      }
-      
-      jQuery(this)
-        .unbind("error")
-        .attr("src", newsrc); 
+(function ($) {
+  var tnurl = '';
+  Drupal.behaviors.ubl_access_rights_from_metadata = {
+    attach: function(context, settings) {
+      tnurl = settings.ubl_access_rights_from_metadata.tn_access_restricted;
     }
-  };
-  jQuery('.islandora-solr-search-result IMG')
-    .on("error", replaceInaccessibleThumbnail)
-    .each(function() {
-      if (this.complete) { 
-        alert('complete: ' + this);
+  }
+  var replaceInaccessibleThumbnail = function() {
+    var _replaceInaccessibleThumbnail = function(img) {
+      if (tnurl === '') {
+        setTimeout(function() { _replaceInaccessibleThumbnail(img) }, 100);
+        return;
       }
-    });
-});
+      var parts = img.src.split("?", 2);
+      if (parts[0].endsWith('datastream/TN/view')) {
+        var newsrc = settings.ubl_access_rights_from_metadata.tn_access_restricted;
+        if (parts.length == 2) {
+          newsrc += '?' + parts[1];
+        }
+        
+        $(img)
+          .unbind("error")
+          .attr("src", newsrc); 
+      }
+    };
+    _replaceInaccessibleThumbnail(this);
+  };
+  $('.islandora-solr-search-result IMG')
+    .on("error", replaceInaccessibleThumbnail);
+})(jQuery);
+
