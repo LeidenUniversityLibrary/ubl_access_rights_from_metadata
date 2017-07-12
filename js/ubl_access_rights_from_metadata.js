48ab8720099bbd2ea46d8dedb4384ec46d8118bf
lucasvanschaik <schaik@library.leidenuniv.nl>
2017-07-12 02:55:10 -0400

    Handle all access restricted TN

--- js/ubl_access_rights_from_metadata.js
+++ js/ubl_access_rights_from_metadata.js
@@ -17,7 +17,7 @@
         return;
       }
       var parts = img.src.split("?", 2);
-      if (parts[0].endsWith('datastream/TN/view')) {
+      if (parts[0].endsWith('datastream/TN/view') || parts[0].endsWith('islandora/images/folder.png')) {
         var newsrc = tnurl;
         if (parts.length == 2) {
           newsrc += '?' + parts[1];
@@ -25,14 +25,16 @@
         
         $(img)
           .unbind("error")
-          .attr("src", newsrc); 
+          .attr("src", newsrc)
+          .parent().find(":hidden").show();
       }
     };
     _replaceInaccessibleThumbnail(this);
   };
   $(document).ready(function() {
-    $('.islandora-solr-search-result IMG')
-      .on("error", replaceInaccessibleThumbnail);
+    $('.islandora-solr-search-result IMG').on("error", replaceInaccessibleThumbnail);
+    $('.islandora-object-thumb IMG').on("error", replaceInaccessibleThumbnail);
+    $('.islandora-compound-thumbs .dc-grid-item IMG').on("error", replaceInaccessibleThumbnail);
   });
 })(jQuery);
 
